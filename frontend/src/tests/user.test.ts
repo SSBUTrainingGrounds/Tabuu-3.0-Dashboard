6edf0e7065e452d35060d789c2daac8a201d03aa
atomflunder <80397293+atomflunder@users.noreply.github.com>
2023-07-17 12:21:22 +0200

    Update tests

--- frontend/src/tests/user.test.ts
+++ frontend/src/tests/user.test.ts
@@ -1,5 +1,6 @@
 import { describe, it, expect } from "vitest";
 import { getUserName, getUserAvatar } from "../helpers/userDetails";
+import { getDefaultUserAvatar } from "@/helpers/mockUsers";
 import type { GuildUser } from "@/helpers/types";
 
 describe("User Details", () => {
@@ -38,10 +39,18 @@ describe("User Details", () => {
             expect(result).toBe("https://cdn.discordapp.com/avatars/123/abc123.png");
         });
 
-        it("should return a random avatar if the user does not exist", () => {
-            const users = new Map<string, GuildUser>();
+        it("should return a certain default avatar if the user does not exist", () => {
+            const result = getUserAvatar(new Map<string, GuildUser>(), "123");
 
-            const result = getUserAvatar(users, "123");
+            expect(result).toBe("https://cdn.discordapp.com/embed/avatars/0.png");
+
+            const result2 = getUserAvatar(new Map<string, GuildUser>(), "5434563432432342234");
+
+            expect(result2).toBe("https://cdn.discordapp.com/embed/avatars/3.png");
+        });
+
+        it("should return a random avatar if the user does not exist", () => {
+            const result = getDefaultUserAvatar("123");
 
             expect(result).toMatch(/https:\/\/cdn.discordapp.com\/embed\/avatars\/\d+.png/);
         });
